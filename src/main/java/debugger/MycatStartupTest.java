3e0304c02e1e1570eec653833455ad32ea358979
tjoe <dongfang3654@outlook.com>
2018-01-02 14:13:47 +0800

    单库模式

---
+++ src/main/java/debugger/MycatStartupTest.java
@@ -0,0 +1,14 @@
+package debugger;
+
+import io.mycat.MycatStartup;
+
+/**
+ * Created by tjoe on 2017/11/24.
+ */
+public class MycatStartupTest {
+
+    public static void main(String[] args) {
+        MycatStartup.main(args);
+    }
+
+}
---
+++ src/main/java/mycryptdb/main/sql_handler/DMLHandler.java
@@ -0,0 +1,7 @@
+package mycryptdb.main.sql_handler;
+
+/**
+ * Created by tjoe on 2018/1/2.
+ */
+public class DMLHandler {
+}
